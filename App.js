cd1e7f7314a10af316b6ff7b7ee74a072c66de8b
Dishant Kumar <dishant.kumar@hashedin.com>
2018-10-24 17:06:49 +0530

    home n profile screen

--- App.js
+++ App.js
@@ -6,44 +6,25 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import {
+  StackNavigator,
+} from 'react-navigation';
+import Home from './src/components/Home/Home';
+import Profile from './src/components/Profile/profile';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const prefix = Platform.OS == 'android' ? 'awesomeproject://awesomeproject/' : 'awesomeproject://';
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
+const AppSatck = StackNavigator({
+  Home: { screen: Home, path: 'home'},
+    
+  Profile: { screen: Profile, path: 'profile' },
+  },
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const App = () => (
+  <AppSatck uriPrefix={prefix} />
+);
+
+export default App;
